3aaf0d39189bbf5a9aadcd6530bd70540d9d6207
yaswanth70 <131728383+yaswanth70@users.noreply.github.com>
2024-08-17 12:03:53 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,3 +13,27 @@ resource "google_compute_subnetwork" "private_subnet" {
   ip_cidr_range = "10.0.2.0/24"
   network       = google_compute_network.vpc_network.id
 }
+resource "google_compute_instance" "web_server" {
+  name         = "web-server"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network       = google_compute_network.vpc_network.id
+    subnetwork    = google_compute_subnetwork.public_subnet.id
+    access_config {
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+    systemctl start nginx
+  EOT
+}
